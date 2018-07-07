18af418c0f63ceb7e1d2ca7ccd8fff62ec39eef5
William Kwak <league@wts21.attlocal.net>
2018-07-07 16:26:36 -0700

    Actually finished module 3 (last one in else if)

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -16,9 +16,19 @@ public class BirthdayReminder {
 		String myBirthday = "April 16th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		String bday = JOptionPane.showInputDialog("Which birthday do you want to show");
 		// 3. Print out what the user typed
+		if (bday.equals("mom")) {
+			JOptionPane.showMessageDialog(null, momsBirthday);
+		}
 		
+		else if (bday.equals("dad")) {
+			JOptionPane.showMessageDialog(null, dadsBirthday);
+		}
+		
+		else { 
+			JOptionPane.showMessageDialog(null, myBirthday);
+		}
 		// 4. if user asked for "mom"
 			//print mom's birthday
 		// 5. if user asked for "dad"
